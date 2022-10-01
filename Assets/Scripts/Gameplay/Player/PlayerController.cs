c5b2454d8f4a36bc96ca13bc4a3896323e7a7e37
LucioRuixo <54895404+LucioRuixo@users.noreply.github.com>
2022-10-01 14:39:09 -0300

    Salto implementado

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Components
+    private Animator animator;
+    #endregion
+
     [SerializeField] private float stepLength = 0.5f;
     [SerializeField] private float stepSpeed = 50f;
 
     private bool moving = false;
     private bool canHide = false;
     private bool isHidden = false;
-    HidingSpot currentHidingSpot = null;
+
+    private HidingSpot currentHidingSpot = null;
     private Vector3 posBeforeHiding;
     private Vector3 lastPosition;
-    private float yPos = 1.0f; //posicion en Y en que se va a mantener siempre le player
 
+    private float yPos = 1.0f; //posicion en Y en que se va a mantener siempre el player
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
 
     private void Start()
     {
@@ -27,18 +37,18 @@ public class PlayerController : MonoBehaviour
 
     private void TakeInput()
     {
-
-        if (moving) return;
-
-        if (Input.GetKeyDown(KeyCode.Space)) StopHiding();
-
-        if (isHidden) return;
-
-        if (Input.GetKeyDown(KeyCode.D)) MoveInDir(Vector3.right);
-        else if (Input.GetKeyDown(KeyCode.A)) MoveInDir(-Vector3.right);
-        else if (Input.GetKeyDown(KeyCode.W)) MoveInDir(Vector3.forward);
-        else if (Input.GetKeyDown(KeyCode.S)) MoveInDir(-Vector3.forward);
-        
+        if (isHidden)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) StopHiding();
+        }
+        else if (!moving)
+        {
+            if (Input.GetKeyDown(KeyCode.W)) MoveInDir(Vector3.forward);
+            else if (Input.GetKeyDown(KeyCode.S)) MoveInDir(-Vector3.forward);
+            else if (Input.GetKeyDown(KeyCode.A)) MoveInDir(-Vector3.right);
+            else if (Input.GetKeyDown(KeyCode.D)) MoveInDir(Vector3.right);
+            else if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        }
     }
 
     private void MoveInDir(Vector3 dir)
@@ -51,13 +61,20 @@ public class PlayerController : MonoBehaviour
         StartCoroutine(Move(new Vector3(pos.x, yPos, pos.z)));
     }     
 
+    private void Jump()
+    {
+        moving = true;
+        animator.SetTrigger("Jump");
+    }
+
     public void OnEnterHidingSpotTrigger(HidingSpot hidingSpot)
     {
         Debug.Log("enter hiding");
         canHide = true;
         if (!moving) Hide();
         currentHidingSpot = hidingSpot;
-    }    
+    }
+
     private void Hide()
     {
         posBeforeHiding = lastPosition;
@@ -76,6 +93,11 @@ public class PlayerController : MonoBehaviour
         currentHidingSpot = null;
     }
 
+    public void OnJumpEnd()
+    {
+        moving = false;
+    }
+
     #region Coroutines
     private IEnumerator Move(Vector3 pos)
     {
@@ -85,6 +107,7 @@ public class PlayerController : MonoBehaviour
         lastPosition = transform.position;
         float distancePerFrame = stepSpeed * Time.fixedDeltaTime;
         Vector3 initialPos = transform.position;
+
         float t = 0.0f;
         while (t < 1.0)
         {
