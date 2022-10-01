58e88c74a2b9160747801e1ac92966e6891d3dfc
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2022-10-01 20:39:46 -0300

    Agregado spawn random de pedestal

--- Assets/Scripts/Gameplay/Stage/Pedestal/Pedestal.cs
+++ Assets/Scripts/Gameplay/Stage/Pedestal/Pedestal.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Pedestal : MonoBehaviour
 {
-    [SerializeField] private Transform PlayerHeldTransformExample;
-    [SerializeField] private Transform PlayerBeforeJumpingTransformExample;
+    [SerializeField] private Transform playerHeldTransformExample;
+    [SerializeField] private Transform playerBeforeJumpingTransformExample;
+    [SerializeField] private float initPosA;
+    [SerializeField] private float initPosB;
+    private void Start()
+    {
+        float x = initPosA;
+        if (Random.Range(0, 2) == 0) x = initPosB;
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            other.GetComponent<PlayerBody>().Controller.OnEnterPedestalTrigger(PlayerBeforeJumpingTransformExample.position);
+            other.GetComponent<PlayerBody>().Controller.OnEnterPedestalTrigger(playerBeforeJumpingTransformExample.position);
         }
     }
 }
