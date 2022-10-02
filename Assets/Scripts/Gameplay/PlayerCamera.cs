eb11ad312fba884fb522be08ddfdb09e32d385d4
LucioRuixo <54895404+LucioRuixo@users.noreply.github.com>
2022-10-02 10:43:02 -0300

    Variables de freeze agregadas a cámara de jugador

--- Assets/Scripts/Gameplay/PlayerCamera.cs
+++ Assets/Scripts/Gameplay/PlayerCamera.cs
@@ -4,18 +4,26 @@ public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] private Transform pivot;
     [Space]
+    [Tooltip("El valor de esta variable solo se evalúa al principio del juego")]
+    [SerializeField] private bool freezeX;
+    [Tooltip("El valor de esta variable solo se evalúa al principio del juego")]
+    [SerializeField] private bool freezeZ;
     [SerializeField] private Vector3 offset;
-    private float initialX;
+
+    private Vector3 initialPosition;
 
     private void Start()
     {
-        initialX = transform.position.x;
+        initialPosition = transform.position;
     }
 
     private void Update()
     {
+        float x = freezeX ? initialPosition.x : pivot.position.x + offset.x;
+        float z = freezeZ ? initialPosition.z : pivot.position.z + offset.z;
+
         Vector3 position = transform.position;
-        position = new Vector3(initialX, pivot.position.y + offset.y, pivot.position.z + offset.z);
+        position = new Vector3(x, initialPosition.y, z);
         transform.position = position;
     }
 }
